6f9e7647f6beb3a56473b1328962b83b2b5a382e
Harish Kumar Gangula <harish@sanketika.in>
2021-07-06 14:12:47 +0530

    Issue #000 fix: cdata concat

--- projects/sunbird-pdf-player/src/lib/sunbird-pdf-player.service.ts
+++ projects/sunbird-pdf-player/src/lib/sunbird-pdf-player.service.ts
@@ -38,9 +38,9 @@ export class SunbirdPdfPlayerService {
           host: context.host || '',
           endpoint: context.endpoint || '/data/v3/telemetry',
           tags: context.tags,
-          cdata: [{ id: this.contentSessionId, type: 'ContentSession' },
+          cdata: (context.cdata || []).concat([{ id: this.contentSessionId, type: 'ContentSession' },
           { id: this.playSessionId, type: 'PlaySession' },
-          {id: "2.0" , type: "PlayerVersion"}]
+          {id: "2.0" , type: "PlayerVersion"}])
         },
         userOrgDetails: {}
       };
@@ -141,9 +141,9 @@ export class SunbirdPdfPlayerService {
         env: 'contentplayer',
         sid: this.context.sid,
         uid: this.context.uid,
-        cdata: [{ id: this.contentSessionId, type: 'ContentSession' },
+        cdata: (this.context.cdata || []).concat([{ id: this.contentSessionId, type: 'ContentSession' },
         { id: this.playSessionId, type: 'PlaySession' },
-        {id: "2.0" , type: "PlayerVersion"}],
+        {id: "2.0" , type: "PlayerVersion"}]),
         rollup: this.context.contextRollup || {}
       }
     });
