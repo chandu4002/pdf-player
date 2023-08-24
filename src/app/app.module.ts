cc14bf13c5adaaaae8b64d9ca91ab8b21e616c48
saichandu <sai.chandu@trigyn.com>
2023-08-24 13:10:39 +0530

    PDF last page navigation changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// import { SunbirdPdfPlayerModule } from '@project-sunbird/sunbird-pdf-player-v9';
-import { SunbirdPdfPlayerModule } from 'dic-pdf-player';
+import { SunbirdPdfPlayerModule } from 'dist/sunbird-pdf-player';
+// import { SunbirdPdfPlayerModule } from '@dictrigyn/pdf-player';
 
 @NgModule({
   declarations: [
